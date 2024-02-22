139070029af8b978d0c6580092fcd9a1e66dc1ce
KLab-UT <utt.klab@gmail.com>
2024-02-21 23:36:34 -0700

    updated mapping_cleaned_reads.sh with new working directory- however, the username for the scratch directory should be something the user provides

--- bash_scripts/mapping_cleaned_reads.sh
+++ bash_scripts/mapping_cleaned_reads.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 {
-usage="$(basename "$0") [-h] [-I <SRA_list_Illumina>] [-N <SRA_list_Nanopore>] [-g <reference_genome>] [-d <working_directory]
+usage="$(basename "$0") [-h] [-I <SRA_list_Illumina>] [-N <SRA_list_Nanopore>] [-g <reference_genome>][-d <repository_directory>] [-w <working_directory]
 This program will trim and map reads from given SRA sequences files to compare mapping of different NGS datasets.
     -h  show this help text
     -I  File or path to SRA accession list for Illumina data in tabular format
     -N  File or path to SRA accession list for Nanopore data in tabular format
     -g  The reference genome (in fasta format). It is assumed that the genome is within the References directory in -d 
-    -d  Path to the working directory (the main directory for the repository)
+    -d  Path to the repository directory (the path to the repository you cloned from github)
+    -w  Path to the working directory (where processed files will be stored)
     -t  Number of CPU processors"
-options=':h:N:I:g:t:d:'
+options=':h:N:I:g:t:d:w:'
 while getopts $options option; do
   case "$option" in
     h) echo "$usage"; exit;;
@@ -18,22 +19,24 @@ while getopts $options option; do
     g) g=$OPTARG;;
     t) t=$OPTARG;;
     d) d=$OPTARG;;
+    w) w=$OPTARG;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
    \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
   esac
 done
 
 echo ""
-echo "Working directory: " $d
-echo "Illumina data:     " $I
-echo "Nanopore data:     " $N
-echo "Genome file:       " $g
-echo "Threads:           " $t
+echo "Repository directory: " $d
+echo "Working directory:    " $w
+echo "Illumina data:        " $I
+echo "Nanopore data:        " $N
+echo "Genome file:          " $g
+echo "Threads:              " $t
 echo ""
 
 # mandatory arguments
-if [ ! "$I" ] || [ ! "$N" ] || [ ! "$g" ] || [ ! "$t" ] || [ ! "$d" ]; then
-  echo "arguments -I, -N, -g, -t, and -d  must be provided"
+if [ ! "$I" ] || [ ! "$N" ] || [ ! "$g" ] || [ ! "$t" ] || [ ! "$d" ] || [ ! "$w" ]; then
+  echo "arguments -I, -N, -g, -t, -d, and -w  must be provided"
   echo "$usage" >&2; exit 1
 fi
 
@@ -42,15 +45,15 @@ begin=`date +%s`
 
 # Setup working environment
 ## -- Illumina
-mkdir -p Illumina/sra_files
-mkdir -p Illumina/raw_reads
-mkdir -p Illumina/cleaned_reads
-mkdir -p Illumina/mapped_reads
+mkdir -p ${w}/Illumina/sra_files
+mkdir -p ${w}/Illumina/raw_reads
+mkdir -p ${w}/Illumina/cleaned_reads
+mkdir -p ${w}/Illumina/mapped_reads
 ## -- Nanopore
-mkdir -p Nanopore/sra_files
-mkdir -p Nanopore/raw_reads
-mkdir -p Nanopore/cleaned_reads
-mkdir -p Nanopore/mapped_reads
+mkdir -p ${w}/Nanopore/sra_files
+mkdir -p ${w}/Nanopore/raw_reads
+mkdir -p ${w}/Nanopore/cleaned_reads
+mkdir -p ${w}/Nanopore/mapped_reads
 
 ##################################################################################
 #  Download fastq files for 22 samples from the Heikema et. al (2020) paper      # 
@@ -58,30 +61,27 @@ mkdir -p Nanopore/mapped_reads
 
 module load sra-toolkit/3.0.2
 
-# COMPLETED    cd ${d}
-# COMPLETED    echo "Downloading Illumina SRA files from the given list of accessions"
-# COMPLETED    cd Illumina/sra_files
-# COMPLETED    prefetch --max-size 800G -O ./ --option-file ${d}/${I}
-# COMPLETED    echo "Converting Illumina SRA files to fastq.gz"
-# COMPLETED    ls -p | grep ERR > sra_dirs
-# COMPLETED    while read i; do mv "$i"*.sra .; rmdir "$i"; done<sra_dirs
-# COMPLETED    SRA= ls -1 *.sra
-# COMPLETED    for SRA in *.sra; do fastq-dump --gzip ${SRA}
-# COMPLETED    done
-# COMPLETED    mv *.fastq.gz ../raw_reads
-# COMPLETED    
-# COMPLETED    cd ${d}
-# COMPLETED    pwd
-# COMPLETED    echo "Downloading Nanopore SRA files from the given list of accessions"
-# COMPLETED    cd Nanopore/sra_files
-# COMPLETED    prefetch --max-size 800G -O ./ --option-file ${d}/${N}
-# COMPLETED    echo "Converting Nanopore SRA files to fastq.gz"
-# COMPLETED    ls -p | grep ERR > sra_dirs
-# COMPLETED    while read i; do mv "$i"*.sra .; rmdir "$i"; done<sra_dirs
-# COMPLETED    SRA= ls -1 *.sra
-# COMPLETED    for SRA in *.sra; do fastq-dump --gzip ${SRA}
-# COMPLETED    done
-# COMPLETED    mv *.fastq.gz ../raw_reads
+echo "Downloading Illumina SRA files from the given list of accessions"
+cd ${w}/Illumina/sra_files
+prefetch --max-size 800G -O ./ --option-file ${d}/${I}
+echo "Converting Illumina SRA files to fastq.gz"
+ls -p | grep ERR > sra_dirs
+while read i; do mv "$i"*.sra .; rmdir "$i"; done<sra_dirs
+SRA= ls -1 *.sra
+for SRA in *.sra; do fastq-dump --gzip ${SRA}
+done
+mv *.fastq.gz ../raw_reads
+
+echo "Downloading Nanopore SRA files from the given list of accessions"
+cd ${w}/Nanopore/sra_files
+prefetch --max-size 800G -O ./ --option-file ${d}/${N}
+echo "Converting Nanopore SRA files to fastq.gz"
+ls -p | grep ERR > sra_dirs
+while read i; do mv "$i"*.sra .; rmdir "$i"; done<sra_dirs
+SRA= ls -1 *.sra
+for SRA in *.sra; do fastq-dump --gzip ${SRA}
+done
+mv *.fastq.gz ../raw_reads
 
 
 module unload sra-toolkit/3.0.2
@@ -101,15 +101,15 @@ echo "Trimming downloaded datasets with fastp."
 echo ""
 
 function trim_reads {
-cd ${d}/$1/raw_reads
+cd ${w}/$1/raw_reads
 ls *.fastq.gz | cut -d "." -f "1" | sort > fastq_list
 while read sample; do fastp -w ${t} -i ${sample}.fastq.gz -o ../cleaned_reads/${sample}_cleaned.fastq.gz
 done<fastq_list
 cd ..
 }
 
-# COMPLETED    trim_reads Illumina
-# COMPLETED    trim_reads Nanopore
+trim_reads Illumina
+trim_reads Nanopore
 
   
 module unload fastp/0.20.1
@@ -122,8 +122,8 @@ module load bwa/2020_03_19
 
 echo "Indexing Reference"
 echo ""
-cd ${d}
-bwa index ${d}/Reference/${g}
+cd ${w}
+bwa index ${w}/Reference/${g}
 
 module unload bwa/2020_03_19
 
@@ -140,8 +140,8 @@ echo ""
 function align_reads {
 cd $1/cleaned_reads
 ls *_cleaned.fastq.gz | awk -F'[._]' '{print $1}' | sort > cleaned_list
-while read sample; do bwa mem ${d}/Reference/${g} ${sample}_cleaned.fastq.gz > ${d}/$1/mapped_reads/${sample}_mapped.sam
-samtools sort ${d}/$1/mapped_reads/${sample}_mapped.sam > ${d}/$1/mapped_reads/${sample}_sorted.bam -@ ${t}
+while read sample; do bwa mem ${w}/Reference/${g} ${sample}_cleaned.fastq.gz > ${w}/$1/mapped_reads/${sample}_mapped.sam
+samtools sort ${w}/$1/mapped_reads/${sample}_mapped.sam > ${w}/$1/mapped_reads/${sample}_sorted.bam -@ ${t}
 done<cleaned_list
 cd ..
 }
